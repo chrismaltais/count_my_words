5bba9c085097586ee5b5700579ae21db12a0587a
Chris Maltais <chris.maltais@hotmail.ca>
2018-01-27 20:22:32 -0500

    Update on app.py to run

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Result
+from models import *
 
 # Both GET and POST because we route to the same palce for both requests
 @app.route('/', methods = ['GET', 'POST'])
