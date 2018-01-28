dafc84bb8072757e47103a0f387fad3aa8c7a937
Chris Maltais <chris.maltais@hotmail.ca>
2018-01-27 21:58:28 -0500

    Fix requests typo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,8 +12,20 @@ from models import *
 
 # Both GET and POST because we route to the same palce for both requests
 @app.route('/', methods = ['GET', 'POST'])
-def hello():
-    return render_template('index.html')
+def index():
+    errors = []
+    results = {}
+    if request.method == "POST":
+        # Get the URL the user has entered
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    return render_template('index.html', errors=errors, results=results)
 
 #@app.route('/<name>')
 #def hello_name(name):
